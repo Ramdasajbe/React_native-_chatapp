cbe4be137f2a2b89f746ff05ee884ba59bdfe2dd
YashKalyani123 <yashkalyani770@gmail.com>
2023-09-05 11:37:57 +0530

    MessageList UI

--- Frontend/App/Component/ChatGroups.js
+++ Frontend/App/Component/ChatGroups.js
@@ -5,19 +5,29 @@ import {
   TouchableOpacity,
   View,
   Alert,
+  BackHandler
 } from 'react-native';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import api from '../API/Api';
-import {getUserFromLocalStorage} from './AsyncStorage';
+import { RemoveUserFromLocalStorage } from './AsyncStorage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const ChatGroups = ({navigation}) => {
+
+
+const ChatGroups = ({ navigation }) => {
   const [Groups, setGroups] = React.useState([]);
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
+
   useEffect(() => {
     getChatGroup();
+    {
+      navigation.setOptions({
+        onclick: {},
+        headerShown: true,
+      })
+    }
   }, []);
+
   const getChatGroup = async () => {
     try {
       let STORAGE_KEY = '@user_input';
@@ -28,20 +38,23 @@ const ChatGroups = ({navigation}) => {
           Authorization: `Bearer ${JSON.parse(user).token}`,
         },
       };
-      const {data} = await axios.get(
+      const { data } = await axios.get(
         'http://192.168.29.243:5000/api/v1/chat',
         Config,
       );
       setGroups(data);
-    } catch (error) {}
+    } catch (error) { }
   };
+
+
+
   return (
-    <View style={{width: windowWidth, display: 'flex', alignItems: 'center'}}>
+    <View style={{ width: windowWidth, display: 'flex', alignItems: 'center' }}>
       <TouchableOpacity
         onPress={() => {
-          navigation.navigate('SingalChat', {data: Groups[0]});
+          navigation.navigate('SingalChat', { data: Groups[0] });
         }}>
-        <Text style={{fontSize: 30}}>{Groups[0]?.chatName}</Text>
+        <Text style={{ fontSize: 30 }}>{Groups[0]?.chatName}</Text>
       </TouchableOpacity>
     </View>
   );
--- Frontend/App/Component/MessageList.js
+++ Frontend/App/Component/MessageList.js
@@ -1,20 +1,29 @@
 import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import moment from 'moment';
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
+
 const MessageList = props => {
   //Message data comes from props
   let messageData = props.message;
   //Flatlist function
   const renderMessage = item => {
+    console.log(item)
     return (
       <View key={item.index} style={styles.mainView}>
-        {<Text style={styles.mainText}>{item.item.message}</Text>}
+        {<View style={styles.mainText}>
+          <Text style={styles.user}>{item.item.sender.username}</Text>
+          <Text style={styles.message}>{item.item.message}</Text>
+          <Text style={styles.time}>{moment(item.item.createdAt).format("DD MM YYYY hh:mm:a")}</Text>
+        </View>}
       </View>
     );
   };
   return (
     <FlatList
+    style={styles.view}
       data={messageData}
       renderItem={item => renderMessage(item)}
       showsVerticalScrollIndicator={false}
@@ -26,6 +35,8 @@ export default MessageList;
 
 const styles = StyleSheet.create({
   mainText: {
+    flex:1,
+    flexDirection:'column',
     color: 'black',
     padding: 10,
     fontSize: 15,
@@ -35,4 +46,18 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 6,
   },
+  user:{
+    fontSize:16,
+    color:'blue',
+    fontWeight:'600'
+  },
+  message:{
+
+  },
+  view:{
+    maxHeight:'85%'
+  },
+  time:{
+    alignSelf:'flex-end'
+  }
 });
